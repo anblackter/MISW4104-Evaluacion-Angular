01f6a5fcd92f9c128ce4f87c3c54a435b97f1bbe
Angel Henao <ay.henao@uniandes.edu.co>
2024-11-01 19:38:12 -0500

    Include tests for Vehicle List Component

--- src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
@@ -3,10 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VehicleListComponent } from './vehicle-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { VehicleService } from '../vehicle.service';
+import { DebugElement } from '@angular/core';
+import { Vehicle } from '../vehicle';
+import { faker } from '@faker-js/faker';
+import {By} from '@angular/platform-browser';
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,12 +21,47 @@ describe('VehicleListComponent', () => {
     })
     .compileComponents();
 
+
+  });
+
+  beforeEach(() =>{
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++){
+      const vehicle = new Vehicle(
+        faker.number.int(),
+        faker.word.words(1),
+        faker.word.words(1),
+        faker.word.words(1),
+        faker.number.int(),
+        faker.number.int(),
+        faker.word.words(1),
+        faker.image.url()
+      );
+      component.vehicles.push(vehicle);
+    }
     fixture.detectChanges();
-  });
+    debug = fixture.debugElement;
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should has a table', () => {
+    expect(debug.query(By.css('table'))).toBeTruthy();
+  });
+
+  it('should has a table with 4 rows', () => {
+    expect(debug.queryAll(By.css('tr')).length).toBe(4);
+  });
+
+  it('should has a table with 1 thead', () => {
+    expect(debug.queryAll(By.css('thead')).length).toBe(1);
+  });
+
+  it('should has a table with 1 tbody', () => {
+    expect(debug.queryAll(By.css('tbody')).length).toBe(1);
   });
 });
